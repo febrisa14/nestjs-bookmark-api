70143d969a7e65d46915d742479da0eafd874d8a
Febrisa Permana <78676283+febrisa14@users.noreply.github.com>
2023-07-16 18:46:37 +0800

    update custom decorator in get user profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/decorator';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-    @UseGuards(JwtGuard)
     @Get()
-    getProfile(@Req() req: Request) {
-        return req.user;
+    getProfile(@GetUser() user: User) {
+        return user;
     }
 }
